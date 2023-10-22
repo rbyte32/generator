d6ce175310552a5a225a53a6e45bb710d4cfabbf
rbyte32 <83476792+rbyte32@users.noreply.github.com>
2023-10-22 19:05:00 +0300

    Create python sadala.py

---
+++ python sadala.py
@@ -0,0 +1,13 @@
+
+
+# unit test for this function test_add.py
+def add(variable_x, variable_y):
+    """
+    
+    Add two numbers together.
+    
+    """
+    return variable_x + variable_y
+
+
+print ("Calculator project")
